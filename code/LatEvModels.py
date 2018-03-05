fix Gaussian Observation Models, implement Y-variance independent of X

code/LatEvModels.py
@@ -206,26 +206,26 @@ class LocallyLinearEvolution(NoisyEvolution):
         
         # L = -0.5*(resX_0^T*Q0^{-1}*resX_0) - 0.5*Tr[resX^T*Q^{-1}*resX] + 0.5*N*log(Det[Q0^{-1}])
         #     + 0.5*N*T*log(Det[Q^{-1}]) - 0.5*N*T*d_X*log(2*Pi)
-        L0 = -0.5*tf.reduce_sum(resX0_Nxd*tf.matmul(resX0_Nxd, self.Q0Inv_dxd), name='L0') 
-        L1 = -0.5*tf.reduce_sum(resX_NTm1xd*tf.matmul(resX_NTm1xd, self.QInv_dxd), name='L1' )
-        L2 = 0.5*tf.log(tf.matrix_determinant(self.Q0Inv_dxd))*tf.cast(Nsamps, DTYPE)
-        L3 = ( 0.5*tf.log(tf.matrix_determinant(self.QInv_dxd))*
+        LX0 = -0.5*tf.reduce_sum(resX0_Nxd*tf.matmul(resX0_Nxd, self.Q0Inv_dxd), name='LX0') 
+        LX1 = -0.5*tf.reduce_sum(resX_NTm1xd*tf.matmul(resX_NTm1xd, self.QInv_dxd), name='LX1' )
+        LX2 = 0.5*tf.log(tf.matrix_determinant(self.Q0Inv_dxd))*tf.cast(Nsamps, DTYPE)
+        LX3 = ( 0.5*tf.log(tf.matrix_determinant(self.QInv_dxd))*
                tf.cast((NTbins-1)*Nsamps, DTYPE) )
-        L4 = -0.5*np.log(2*np.pi)*tf.cast(Nsamps*NTbins*xDim, DTYPE)
+        LX4 = -0.5*np.log(2*np.pi)*tf.cast(Nsamps*NTbins*xDim, DTYPE)
         
-        LatentDensity = L0 + L1 + L2 + L3 + L4
+        LatentDensity = LX0 + LX1 + LX2 + LX3 + LX4
         
         self.LX_summ = tf.summary.scalar('LogDensity_Xterms', LatentDensity)
-        self.LX1_summ = tf.summary.scalar('LX1', L1)
+        self.LX1_summ = tf.summary.scalar('LX1', LX1)
                 
-        return LatentDensity, [L0, L1, L2, L3, L4] 
+        return LatentDensity, [LX0, LX1, LX2, LX3, LX4] 
     
 
     #** The methods below take a session as input and are not part of the main
     #** graph. They should only be used standalone.
 
     def sample_X(self, sess, Xvar_name, Nsamps=2, NTbins=3, X0data=None, with_inflow=False,
-                 path_mse_threshold=0.5, draw_plots=False, init_variables=True):
+                 path_mse_threshold=0.1, draw_plots=False, init_variables=True):
         """
         Runs forward the stochastic model for the latent space.
          

code/ObservationModels.py
@@ -27,6 +27,7 @@ else:
 
 TEST_DIR = '/Users/danielhernandez/work/supervind/tests/test_results/'
 
+DTYPE = tf.float32
 
 class ObsModel():
     """
@@ -149,13 +150,8 @@ class PoissonObs(ObsModel):
         
         rate_NTxD = self.rate_NTxD
         rate = sess.run(rate_NTxD, feed_dict={Xvar_name : Xdata_NxTxd})
-        print('Sampled rate (mean, std, max)\n', np.mean(rate), np.std(rate),
-              np.max(rate))
-
         rate = np.reshape(rate, [Nsamps, NTbins, self.yDim])
         Ydata_NxTxD = np.random.poisson(rate)
-        print('Ydata (mean, std, max)\n', np.mean(Ydata_NxTxD), np.std(Ydata_NxTxD),
-              np.max(Ydata_NxTxD))
         
         return Ydata_NxTxD, Xdata_NxTxd
     
@@ -168,7 +164,7 @@ class GaussianObs():
         """
         ObsModel.__init__(self, Y, X, params, lat_ev_model, is_out_positive)
         
-        self.MuY_NxTxD, self.SigmaInvY_NxTxDxD = self._define_mean_variance()
+        self.MuY_NxTxD, self.SigmaInvY_DxD = self._define_mean_variance()
         self.LogDensity, self.checks = self.compute_LogDensity() # self.checks meant for debugging
     
     def _define_mean_variance(self, Input=None):
@@ -183,26 +179,29 @@ class GaussianObs():
 
         Input = tf.reshape(Input, [Nsamps*NTbins, xDim], name='X_input')
         
-        rangeY = self.params.initrange_outY
+        rangeY = self.params.initrange_Goutmean
+        initSigma = self.params.initrange_Goutvar
+        init_b = self.params.initbias_Goutmean
         obs_nodes = 64
         fully_connected_layer = FullLayer()
         with tf.variable_scope("obs_nn_mean", reuse=tf.AUTO_REUSE):
-            full1 = fully_connected_layer(Input, obs_nodes, 'softplus', 'full1')
-            full2 = fully_connected_layer(full1, obs_nodes, 'softplus', 'full2')
+            full1 = fully_connected_layer(Input, obs_nodes, 'softplus', 'full1',
+                                          initializer=tf.random_normal_initializer(stddev=0.5))
+            full2 = fully_connected_layer(full1, obs_nodes, 'softplus', 'full2',
+                                          initializer=tf.random_normal_initializer(stddev=0.5))
             MuY_NTxD = fully_connected_layer(full2, yDim, 'linear', 'output',
-                                          initializer=tf.random_uniform_initializer(-rangeY, rangeY))
+                                             initializer=tf.random_uniform_initializer(-rangeY, rangeY),
+                                             b_initializer=tf.random_normal_initializer(init_b) )
             MuY_NxTxD = tf.reshape(MuY_NTxD, [Nsamps, NTbins, yDim])
-        with tf.variable_scope("obs_nn_var", reuse=tf.AUTO_REUSE):
-            full1 = fully_connected_layer(Input, obs_nodes, 'softplus', 'full1')
-            full2 = fully_connected_layer(full1, obs_nodes, 'softplus', 'full2')
-            SigmaInvChol_NTxD2 = fully_connected_layer(full2, yDim**2, 'linear', 'output',
-                                          initializer=tf.random_uniform_initializer(-rangeY, rangeY))
-            self.SigmaInvChol_NTxDxD = tf.reshape(SigmaInvChol_NTxD2, [Nsamps*NTbins, yDim, yDim])
-            SigmaInv_NTxDxD = tf.matmul(self.SigmaInvChol_NTxDxD, self.SigmaInvChol_NTxDxD,
+        with tf.variable_scope("obs_var", reuse=tf.AUTO_REUSE):
+            SigmaInvChol_DxD = tf.get_variable('SigmaInvChol', 
+                                                initializer=tf.cast(initSigma*tf.eye(yDim), DTYPE))
+            self.SigmaChol_DxD = tf.reshape(tf.matrix_inverse(SigmaInvChol_DxD),
+                                                [Nsamps, NTbins, yDim, yDim]) # Needed only for sampling
+            SigmaInv_DxD = tf.matmul(SigmaInvChol_DxD, SigmaInvChol_DxD,
                                         transpose_b=True)
-            SigmaInv_NxTxDxD = tf.reshape(SigmaInv_NTxDxD, [Nsamps, NTbins, yDim, yDim])
             
-        return MuY_NxTxD, SigmaInv_NxTxDxD 
+        return MuY_NxTxD, SigmaInv_DxD 
         
     def compute_LogDensity(self, Input=None, with_inflow=False):
         """
@@ -212,26 +211,27 @@ class GaussianObs():
             Nsamps = self.Nsamps
             NTbins = self.NTbins
             X = self.X
-            LX, Xchecks = self.lat_ev_model.compute_LogDensity_Xterms(with_inflow=with_inflow)
-            MuY_NxTxD, SigmaInv_NxTxDxD = self.MuY_NxTxD, self.SigmaInvY_NxTxDxD
+            LX, checks = self.lat_ev_model.compute_LogDensity_Xterms(with_inflow=with_inflow) # checks=[LX0, LX1, LX2, LX3, LX4]
+            MuY_NxTxD, SigmaInvY_DxD = self.MuY_NxTxD, self.SigmaInvY_DxD
         else:
             Nsamps = tf.shape(Input)[0]
             NTbins = tf.shape(Input)[1]
             X = Input
-            LX, Xchecks = self.lat_ev_model.compute_LogDensity_Xterms(X, with_inflow=with_inflow)        
-            MuY_NxTxD, SigmaInv_NxTxDxD = self._define_mean_variance(X)
+            LX, checks = self.lat_ev_model.compute_LogDensity_Xterms(X, with_inflow=with_inflow) # checks=[LX0, LX1, LX2, LX3, LX4]
+            MuY_NxTxD, SigmaInvY_DxD = self._define_mean_variance(X)
         
+        SigmaInvY_NTxDxD = tf.tile(tf.expand_dims(SigmaInvY_DxD, axis=0), [Nsamps*NTbins, 1, 1])
         MuY_NTx1xD = tf.reshape(MuY_NxTxD, [Nsamps*NTbins, 1, yDim])
-        SigmaInv_NTxDxD = tf.reshape(SigmaInv_NxTxDxD, [Nsamps*NTbins, yDim, yDim])
         Y_NTx1xD = tf.reshape(self.Y, [Nsamps*NTbins, 1, yDim])
         
-        DeltaY = Y_NTx1xD - MuY_NTx1xD
+        DeltaY_NTx1xD = Y_NTx1xD - MuY_NTx1xD
         
-        L1 = -0.5*tf.reduce_sum(DeltaY*tf.matmul(DeltaY, SigmaInv_NTxDxD))
-        L2 = 0.5*tf.reduce_sum(tf.log(tf.matrix_determinant(SigmaInv_NTxDxD)))
-        LY = L1 + L2
+        LY1 = -0.5*tf.reduce_sum(DeltaY_NTx1xD*tf.matmul(DeltaY_NTx1xD, SigmaInvY_NTxDxD))
+        LY2 = 0.5*tf.reduce_sum(tf.log(tf.matrix_determinant(SigmaInvY_DxD)))*tf.cast(Nsamps*NTbins, DTYPE)
+        LY = LY1 + LY2
         
-        return tf.add(LX, LY, name='LogDensity'), [LY, LX, L1, L2, Xchecks]
+        checks.extend([LX, LY, LY1, LY2])
+        return tf.add(LX, LY, name='LogDensity'), checks
 
     #** These methods take a session as input and are not part of the main
     #** graph. They are meant to be used as standalone.
@@ -252,10 +252,10 @@ class GaussianObs():
                                            init_variables=init_variables)
         
         MuY_NxTxD = self.MuY_NxTxD
-        SigmaInvChol = tf.reshape(self.SigmaInvChol_NTxDxD, [Nsamps, NTbins, yDim, yDim])
-        noise_NxTxD = tf.random_normal([Nsamps, NTbins, 1, yDim])
+        SigmaChol_DxD = self.SigmaChol_DxD
+        noise_NxTx1xD = tf.random_normal([Nsamps, NTbins, 1, yDim])
         
-        sampleY_NxTxD = MuY_NxTxD + tf.reshape(tf.matmul(noise_NxTxD, SigmaInvChol),
+        sampleY_NxTxD = MuY_NxTxD + tf.reshape(tf.matmul(noise_NxTx1xD, SigmaChol_DxD),
                                                [Nsamps, NTbins, yDim])
         Ydata_NxTxD = sess.run(sampleY_NxTxD, feed_dict={Xvar_name : Xdata_NxTxd})
         

code/Optimizer_VAEC.py
@@ -18,7 +18,7 @@ import os
 import numpy as np
 import tensorflow as tf
 
-from .ObservationModels import PoissonObs
+from .ObservationModels import PoissonObs, GaussianObs
 from .RecognitionModels import SmoothingNLDSTimeSeries
 from .datetools import addDateTime
 
@@ -27,10 +27,15 @@ DTYPE = tf.float32
 class Optimizer_TS():
     """
     """
-    def __init__(self, params, ObsModel=PoissonObs, RecModel=SmoothingNLDSTimeSeries):
+    def __init__(self, params):
         """
         """        
         self.params = params
+
+        gen_mod_classes = {'Poisson' : PoissonObs, 'Gaussian' : GaussianObs}
+
+        ObsModel = gen_mod_classes[params.gen_mod_class]
+        RecModel = SmoothingNLDSTimeSeries
 
         self.xDim = xDim = params.xDim
         self.yDim = yDim = params.yDim
@@ -76,12 +81,13 @@ class Optimizer_TS():
     def cost_ELBO(self, with_inflow=False):
          
         postX = self.mrec.postX
-        LogDensity, _ = self.mgen.compute_LogDensity(postX, with_inflow=with_inflow)
+        LogDensity, checks = self.mgen.compute_LogDensity(postX, with_inflow=with_inflow) # checks=[LX0, LX1, LX2, LX3, LX4, LX, LY, LY1, LY2]
         Entropy = self.mrec.compute_Entropy(postX)
-                
-        return -(LogDensity + Entropy), (LogDensity, Entropy)
+        checks.extend([LogDensity, Entropy])
+        
+        return -(LogDensity + Entropy), checks 
 
-    def train(self, sess, rlt_dir, Ytrain, Yvalid=None, num_epochs=1000):
+    def train(self, sess, rlt_dir, Ytrain, Yvalid=None, num_epochs=2000):
         """
         Initialize all variables outside this method.
         """

code/RecognitionModels.py
@@ -103,7 +103,6 @@ class SmoothingNLDSTimeSeries(GaussianRecognition):
         
         self.Entropy = self.compute_Entropy()
 
-    
     def _compute_TheChol_postX(self, InputX, InputY=None):
         """
         """
@@ -152,15 +151,15 @@ class SmoothingNLDSTimeSeries(GaussianRecognition):
         AQInvsAQInv_NxTxdxd = tf.concat([AQInvsA_NxTm1xdxd, QInvs_Nx1xdxd], axis=1)
         
         # Add in the covariance coming from the observations
-        self.AA_NxTxdxd = Lambda_NxTxdxd + AQInvsAQInv_NxTxdxd
-        self.BB_NxTm1xdxd = tf.reshape(AQInvs_NTm1xdxd, [Nsamps, NTbins-1, xDim, xDim])        
+        AA_NxTxdxd = Lambda_NxTxdxd + AQInvsAQInv_NxTxdxd
+        BB_NxTm1xdxd = tf.reshape(AQInvs_NTm1xdxd, [Nsamps, NTbins-1, xDim, xDim])        
         
         # Computation of the Cholesky decomposition for the total covariance
         aux_fn1 = lambda _, seqs : blk_tridiag_chol(seqs[0], seqs[1])
         TheChol_2xNxTxdxd = tf.scan(fn=aux_fn1, 
-                    elems=[self.AA_NxTxdxd, self.BB_NxTm1xdxd],
-                    initializer=[tf.zeros_like(self.AA_NxTxdxd[0]), 
-                                 tf.zeros_like(self.BB_NxTm1xdxd[0])] )
+                    elems=[AA_NxTxdxd, BB_NxTm1xdxd],
+                    initializer=[tf.zeros_like(AA_NxTxdxd[0]), 
+                                 tf.zeros_like(BB_NxTm1xdxd[0])] )
         
         # TODO: Include an option to turn off the computation of gradterm.
         
@@ -221,7 +220,7 @@ class SmoothingNLDSTimeSeries(GaussianRecognition):
                     initializer=tf.zeros_like(LambdaMu_NxTxd[0], dtype=DTYPE),
                     name='postX' )      # tensorflow triple axel! :)
         
-        return TheChol_2xNxTxdxd, postX, (A_NTxdxd, self.AA_NxTxdxd, postX_gradterm_NxTxd)
+        return TheChol_2xNxTxdxd, postX, [A_NTxdxd, AA_NxTxdxd, BB_NxTm1xdxd, postX_gradterm_NxTxd]
 
 
     def sample_postX(self):

code/layers.py
@@ -31,7 +31,7 @@ class FullLayer():
         self.collections = collections
         self.nl_dict = {'softplus' : tf.nn.softplus, 'linear' : tf.identity,
                         'softmax' : tf.nn.softmax, 'relu' : tf.nn.relu,
-                       'sigmoid' : tf.nn.sigmoid}
+                        'sigmoid' : tf.nn.sigmoid}
         
     def __call__(self, Input, nodes, nl='softplus', scope=None,
                  initializer=tf.orthogonal_initializer(),

tests/test_ObservationModels.py
@@ -29,10 +29,13 @@ flags.DEFINE_integer('xDim', 2, "")
 flags.DEFINE_float('learning_rate', 2e-3, "")
 flags.DEFINE_float('initrange_MuX', 0.2, "")
 flags.DEFINE_float('initrange_B', 3.0, "")
-flags.DEFINE_float('init_Q0', 0.2, "")
-flags.DEFINE_float('init_Q', 1.0, "")
+flags.DEFINE_float('init_Q0', 1.0, "")
+flags.DEFINE_float('init_Q', 2.0, "")
 flags.DEFINE_float('alpha', 0.5, "")
 flags.DEFINE_float('initrange_outY', 3.0,"")
+flags.DEFINE_float('initrange_Goutmean', 0.03,"")
+flags.DEFINE_float('initrange_Goutvar', 1e-1,"")
+flags.DEFINE_float('initbias_Goutmean', 1.0,"")
 params = tf.flags.FLAGS
 
 
@@ -91,69 +94,88 @@ class PoissonObsTest(tf.test.TestCase):
 
         Checks for the Gaussian mean and variance
         """
+        print('X (mean, std, max)', np.mean(self.sampleX3), np.std(self.sampleX3),
+                  np.max(self.sampleX3))
         print('Y (mean, std, max)', np.mean(self.sampleY3), np.std(self.sampleY3),
                   np.max(self.sampleY3))
-        print('')
-            
-    def test_logdensity(self):
-        """
-        Computes the LogDensity and checks that its values are reasonable. In
-        particular, for a particular generative model (`mgen1` above), it compares
-        the LD on external data with the LD on data it itself generates. The
-        latter should be smaller than the former.
-        """
         with self.sess.as_default():
-            ld1_val = self.sess.run(self.ld1, feed_dict={'M1/X1:0' : self.sampleX1,
-                                                    'M1/Y1:0' : self.sampleY1})
-            cks1_val = self.sess.run(self.checks1, feed_dict={'M1/X1:0' : self.sampleX1,
-                                                    'M1/Y1:0' : self.sampleY1})
-            ld1_val2 = self.sess.run(self.ld1, feed_dict={'M1/X1:0' : self.sampleX2,
-                                                     'M1/Y1:0' : self.sampleY2})
-            cks2_val = self.sess.run(self.checks1, feed_dict={'M1/X1:0' : self.sampleX2,
-                                                    'M1/Y1:0' : self.sampleY2})
-            print('LogD << LogD with wrong data:', np.abs(ld1_val), '<<', abs(ld1_val2))
-            print('checks1', cks1_val)
-            print('checks2', cks2_val)
-            print('\n')
-            
+            SigmaChol = self.sess.run(self.mgen3.SigmaChol_NxTxDxD, feed_dict={'M3/X3:0' : self.sampleX3})
+            print('\nSigmaChol (mean, std)', np.mean(SigmaChol), np.std(SigmaChol))
+            mins, maxs = np.min(SigmaChol, axis=(0,1)).flatten(), np.max(SigmaChol, axis=(0,1)).flatten()
+            print('\nSigmaChol ranges')
+            for elem in list(zip(mins, maxs))[:10]: print(elem)
+            print('\nYpath')
+            for i, yvals in enumerate(self.sampleY3[0]): print(i, ',', yvals[:3])
+            print('\nXpath')
+            for i, xvals in enumerate(self.sampleX3[0]): print(i, ',', xvals[:3])
+            print('')
+
+    
+#     def test_logdensity(self):
+#         """
+#         Computes the LogDensity and checks that its values are reasonable. In
+#         particular, for a particular generative model (`mgen1` above), it compares
+#         the LD on external data with the LD on data it itself generates. The
+#         latter should be smaller than the former.
+#         """
+#         with self.sess.as_default():
+#             ld1_val = self.sess.run(self.ld1, feed_dict={'M1/X1:0' : self.sampleX1,
+#                                                     'M1/Y1:0' : self.sampleY1})
+#             cks1_val = self.sess.run(self.checks1, feed_dict={'M1/X1:0' : self.sampleX1,
+#                                                     'M1/Y1:0' : self.sampleY1})
+#             ld1_val2 = self.sess.run(self.ld1, feed_dict={'M1/X1:0' : self.sampleX2,
+#                                                      'M1/Y1:0' : self.sampleY2})
+#             cks2_val = self.sess.run(self.checks1, feed_dict={'M1/X1:0' : self.sampleX2,
+#                                                     'M1/Y1:0' : self.sampleY2})
+#             print('LogD << LogD with wrong data:', np.abs(ld1_val), '<<', abs(ld1_val2))
+#             print('checks1', cks1_val)
+#             print('checks2', cks2_val)
+#             print('\n')
+#             
     def test_poisson_obvs(self):
         """
         Checks for the Poisson observations (integers).
         """
         with self.sess.as_default():
-            print('sampleX 1 (mean, std)', np.mean(self.sampleX1), np.std(self.sampleX1))
+            print('sampleX 1 (mean, std)', np.mean(self.sampleX1), np.std(self.sampleX1),
+                  np.max(self.sampleX1))
             print('Ydata 1 (mean, std, max)', np.mean(self.sampleY1), np.std(self.sampleY1),
                   np.max(self.sampleY1))
-            print('sampleX 2 (mean, std)', np.mean(self.sampleX2), np.std(self.sampleX2))
+            print('sampleX 2 (mean, std)', np.mean(self.sampleX2), np.std(self.sampleX2),
+                  np.max(self.sampleX2))
             print('Ydata 2 (mean, std, max)', np.mean(self.sampleY2), np.std(self.sampleY2),
                   np.max(self.sampleY2))
-            print('\n')
-            
-    def test_eval_Y_and_rates_ispositive(self):
-        """
-        Checks for the Poisson rates with a softplus final layer in the
-        generating NN that yields positive numbers directly.
-        """
-        with self.sess.as_default():
-            rate2_NTxD = self.mgen2.rate_NTxD
-            sample_rate2 = self.sess.run(rate2_NTxD, feed_dict={'M2/X2:0' : self.sampleX2})        
-            print('Rate 2 (mean, std, max)', np.mean(sample_rate2), np.std(sample_rate2),
-                  np.max(sample_rate2))
-            print('')
-            
-    def test_eval_Y_and_rates(self):
-        """
-        Checks for the Poisson rates with a linear final layer in the generating
-        NN that needs to be further composed with an exponential activation to
-        obtain positive rates
-        """
-        with self.sess.as_default():
-            rate1_NTxD = self.mgen1.rate_NTxD            
-            sample_rate1 = self.sess.run(rate1_NTxD, feed_dict={'M1/X1:0' : self.sampleX1})
-            print('Rate 1 (mean, std, max)', np.mean(sample_rate1), np.std(sample_rate1),
-                  np.max(sample_rate1))
-            print('')
+            print('\nYpath')
+            for yvals in self.sampleY1[0]: print(yvals[:3])
 
+            print('\n')
+#             
+#     def test_eval_Y_and_rates_ispositive(self):
+#         """
+#         Checks for the Poisson rates with a softplus final layer in the
+#         generating NN that yields positive numbers directly.
+#         """
+#         with self.sess.as_default():
+#             rate2_NTxD = self.mgen2.rate_NTxD
+#             sample_rate2 = self.sess.run(rate2_NTxD, feed_dict={'M2/X2:0' : self.sampleX2})        
+#             print('Rate 2 (mean, std, max)', np.mean(sample_rate2), np.std(sample_rate2),
+#                   np.max(sample_rate2))
+#             print('')
+#             
+#     def test_eval_Y_and_rates(self):
+#         """
+#         Checks for the Poisson rates with a linear final layer in the generating
+#         NN that needs to be further composed with an exponential activation to
+#         obtain positive rates
+#         """
+#         with self.sess.as_default():
+#             rate1_NTxD = self.mgen1.rate_NTxD            
+#             sample_rate1 = self.sess.run(rate1_NTxD, feed_dict={'M1/X1:0' : self.sampleX1})
+#             print('Rate 1 (mean, std, max)', np.mean(sample_rate1), np.std(sample_rate1),
+#                   np.max(sample_rate1))
+#             print('')
+#     """
+    
 if __name__ == '__main__':
     tf.test.main()
 

tests/tests_data.py
@@ -28,12 +28,13 @@ import tensorflow as tf
 from code.Optimizer_VAEC import Optimizer_TS
 
 DTYPE = tf.float32
-DATA_FILE = '/Users/danielhernandez/work/vind/data/poisson_data_002/datadict'
-# DATA_FILE = '/Users/danielhernandez/work/supervind/data/poisson002/datadict'
+# DATA_FILE = '/Users/danielhernandez/work/vind/data/poisson_data_002/datadict'
+DATA_FILE = '/Users/danielhernandez/work/supervind/data/gaussian001/datadict'
 
 # For information on these parameters, see runner.py
 flags = tf.app.flags
-flags.DEFINE_integer('yDim', 10, "")
+flags.DEFINE_string('gen_mod_class', 'Gaussian', "")
+flags.DEFINE_integer('yDim', 20, "")
 flags.DEFINE_integer('xDim', 2, "")
 flags.DEFINE_float('learning_rate', 2e-3, "")
 flags.DEFINE_float('initrange_MuX', 0.2, "")
@@ -42,6 +43,10 @@ flags.DEFINE_float('init_Q0', 0.5, "")
 flags.DEFINE_float('init_Q', 2.0, "")
 flags.DEFINE_float('alpha', 0.5, "")
 flags.DEFINE_float('initrange_outY', 3.0,"")
+flags.DEFINE_float('initrange_LambdaX', 1.0,"")
+flags.DEFINE_float('initrange_Goutmean', 0.03, "")
+flags.DEFINE_float('initrange_Goutvar', 1.0, "")
+flags.DEFINE_float('initbias_Goutmean', 1.0, "")
 params = tf.flags.FLAGS
 
 
@@ -51,6 +56,7 @@ class DataTests(tf.test.TestCase):
     with open(DATA_FILE, 'rb+') as f:
         datadict = pickle.load(f, encoding='latin1') # `encoding='latin1'` for python 2 pickled data 
         Ydata = datadict['Ytrain']
+        Yvalid = datadict['Yvalid']
         
     yDim = Ydata.shape[2]
     xDim = 2
@@ -66,6 +72,42 @@ class DataTests(tf.test.TestCase):
             
             sess.run(tf.global_variables_initializer())
             
+    def test_postX(self):
+        sess = self.sess
+        with sess.as_default():
+            MuX = sess.run(self.mrec.Mu_NxTxd, feed_dict={'VAEC/Y:0' : self.Ydata})
+            MuX_valid = sess.run(self.mrec.Mu_NxTxd, feed_dict={'VAEC/Y:0' : self.Yvalid})
+            postX = sess.run(self.mrec.postX, feed_dict={'VAEC/Y:0' : self.Ydata,
+                                                         'VAEC/X:0' : MuX})
+            postX_valid = sess.run(self.mrec.postX, feed_dict={'VAEC/Y:0' : self.Yvalid,
+                                                               'VAEC/X:0' : MuX_valid})
+            SigmaY = sess.run(self.mgen.SigmaInvY_DxD)
+            
+            cost = sess.run(self.opt.cost, feed_dict={'VAEC/X:0' : MuX,
+                                                      'VAEC/Y:0' : self.Ydata})
+            checks = sess.run(self.opt.checks, feed_dict={'VAEC/X:0' : MuX,
+                                                          'VAEC/Y:0' : self.Ydata})
+            new_valid_cost = sess.run(self.opt.cost, feed_dict={'VAEC/X:0' : MuX_valid,
+                                                            'VAEC/Y:0' : self.Yvalid})
+            checks_v = sess.run(self.opt.checks, feed_dict={'VAEC/X:0' : MuX_valid,
+                                                          'VAEC/Y:0' : self.Yvalid})
+#             checks2 = sess.run(self.mrec.checks1, feed_dict={'VAEC/X:0' : MuX_valid,
+#                                                           'VAEC/Y:0' : self.Yvalid})
+            
+            print('SigmaY', SigmaY[0,0])
+            print('SigmaY', np.linalg.det(SigmaY))
+            
+            print('\npostX (mean, std)', np.mean(postX), np.std(postX))
+            mins, maxs = np.min(postX, axis=(0,1)), np.max(postX, axis=(0,1))
+            print('postX ranges', list(zip(mins, maxs)))
+            print('postX_valid (mean, std)', np.mean(postX_valid), np.std(postX_valid))
+            mins, maxs = np.min(postX_valid, axis=(0,1)), np.max(postX_valid, axis=(0,1))
+            print('postX_valid ranges', list(zip(mins, maxs)))
+            
+            print('cost', cost, checks)
+            print('valid cost', new_valid_cost, checks_v)
+            print('')
+
     def test_data(self):
         print('Y (mean, std):', np.mean(self.Ydata), np.std(self.Ydata))
         print('Y range:', np.min(self.Ydata), np.max(self.Ydata))